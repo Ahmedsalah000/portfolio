870cc39b50902bcb8d9fd5f8b625aa64e752e6a9
Ahmed Aboshendy <a7meddon123@gmail.com>
2024-10-09 20:52:13 +0300

    cv mod

--- app/layout.tsx
+++ app/layout.tsx
@@ -37,6 +37,7 @@ export default function RootLayout({
             <ThemeSwitch />
           </ActiveSectionContextProvider>
         </ThemeContextProvider>
+        
       </body>
     </html>
   );
